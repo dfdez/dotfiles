8fc603a5d10264f36bfd5db20f34c25f8432dd9e
CodexJs <this.codexjs@gmail.com>
2020-11-07 19:33:14 +0100

    New dotfiles

---
+++ setup/init.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get install zsh -y
+sudo apt-get install stow -y
+sudo apt-get install wget -y
---
+++ setup/plugin.sh
@@ -0,0 +1,19 @@
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s $(which zsh)
+
+#zsh-autosuggestions zsh-completions zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+cp ./.zshrc ~/.zshrc
+
+#fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+
+#nvm installation
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+nvm install node
