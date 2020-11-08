f589d0846631c3718b01243ef45902c1a20b76bb
CodexJs <this.codexjs@gmail.com>
2020-11-08 02:09:08 +0100

    init.sh changes

--- init.sh
+++ init.sh
@@ -1,22 +1,36 @@
-sudo apt update
-sudo apt install git zsh stow wget -y
+apt update
+apt install git zsh stow wget curl -y
 
+# Install OhMyZSH
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s $(which zsh)
 
-#zsh-autosuggestions zsh-completions zsh-syntax-highlighting
+# zsh-autosuggestions zsh-completions zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-#fzf
+# fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 
-#nvm installation
+# nvm installation
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
+# Install node
 nvm install node
 
-stow --adopt --ignore init.sh --ignore .git -t ~ .
+# vim
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
+tar -xf nvim-linux64.tar.gz
+cp -R ./nvim-linux64/* /usr
+rm -r nvim-linux64*
+
+# vim plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Apply dotfile
+rm ~/.zshrc
+stow --ignore init.sh --ignore .git -t ~ .
